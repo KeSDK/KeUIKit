465f1781654ad69470ec0ac8caf846f2890ccb8b
kejianfang <673080885@qq.com>
2018-06-06 21:39:31 +0800

    proj

--- KeSDKPro/ViewController.m
+++ KeSDKPro/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "KeUILable.h"
+#import "UIView+Corner.h"
+#import "UIView+GradientColor.h"
+#import "KeUIKitMacros.h"
 
 @interface ViewController ()
 
@@ -16,14 +20,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *bt = [[UIButton alloc] initWithFrame:CGRectMake(20, 200, 80, 40)];
+    [self.view addSubview:bt];
+    [bt setTitle:@"click" forState:UIControlStateNormal];
+    [bt setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [bt addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+
+    
 }
 
+- (IBAction)click:(id)sender
+{
+    KeUILable *keLable = [[KeUILable alloc] initWithFrame:CGRectMake(100, 150, 100, 40)];
+    [self.view addSubview:keLable];
+//    [keLable setMaxUIWidth:100];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [keLable setText:@"课kkkk"];
+    
+    UILabel *jfv = [[UILabel alloc] initWithFrame:CGRectMake(150, 320, 120, 40)];
+    
+    [self.view addSubview:jfv];
+    
+    jfv.textAlignment =  NSTextAlignmentCenter;
+    [jfv setCornerWithSize:CGSizeMake(20, 20) byRoundingCorners:UIRectCornerTopLeft|UIRectCornerBottomLeft|UIRectCornerBottomRight];
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = jfv.bounds;
+    gradient.startPoint = CGPointMake(0, 0.5);
+    gradient.endPoint = CGPointMake(1, 0.5);
+    gradient.colors = [NSArray arrayWithObjects:
+                       (id)HEXACOLOR(0xFFD000, 1.0).CGColor,
+                       (id)HEXACOLOR(0xFFA000, 1.0).CGColor,
+                       nil];
+    [jfv.layer addSublayer:gradient];
+    [jfv setText:@"联系不到商家"];
 }
-
 
 @end
