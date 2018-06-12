c694121de132a30b2de31b5c9016d2f0dce1bcf8
kejianfang <673080885@qq.com>
2018-06-12 17:43:00 +0800

    增加自定义window悬浮窗口

---
+++ KeSDKPro/KeUIKit/WindowKit/KeDragWindow.h
@@ -0,0 +1,15 @@
+//
+//  KeDragWindow.h
+//  KeSDKPro
+//
+//  Created by 柯建芳 on 2018/6/12.
+//  Copyright © 2018年 柯建芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeDragWindow : UIWindow
+
+- (instancetype)initWindowWithFrame:(CGRect)frame ContentView:(UIView *)view;
+
+@end
---
+++ KeSDKPro/KeUIKit/WindowKit/KeDragWindow.m
@@ -0,0 +1,22 @@
+//
+//  KeDragWindow.m
+//  KeSDKPro
+//
+//  Created by 柯建芳 on 2018/6/12.
+//  Copyright © 2018年 柯建芳. All rights reserved.
+//
+
+#import "KeDragWindow.h"
+
+@implementation KeDragWindow
+
+- (instancetype)initWindowWithFrame:(CGRect)frame ContentView:(UIView *)view
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
